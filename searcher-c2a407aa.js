aaaa33737e6417db764a007ad5776d0ac7a0b649
kmdouglass <kmdouglass@users.noreply.github.com>
2026-01-15 08:23:34 +0000

    deploy: e81d6912deb3c8524f0360d6d9655a8298b17e8f

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-fa6f66e3.js',
+                path_to_root + 'searchindex-44f3fe89.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
